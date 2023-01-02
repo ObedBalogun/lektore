a725ff5d56d2d2d227bf2b52af9c6ccad1160c80
obed <b.obed@yahoo.com>
2023-01-02 23:30:42 +0100

    Update: Email content update

--- app/viewsets.py
+++ app/viewsets.py
@@ -31,23 +31,3 @@ class OTPViewSet(viewsets.ViewSet):
             if response.get("error", None)
             else status.HTTP_200_OK
         )
-
-
-    @action(detail=False, methods=["get"], url_path="is-verified")
-    def is_otp_verified(self, request):
-        user_email = request.GET.get('email')
-        response = ""
-        try:
-            verification_model = UserVerificationModel.objects.get(email=user_email).otp_is_verified
-            response = dict(message=f'{user_email} verification status is {verification_model}')
-        except UserVerificationModel.DoesNotExist:
-            response = dict(error=f"User with {user_email} not found")
-        return ResponseManager.handle_response(
-            errors=response.get("error", None),
-            message=response.get("success", None),
-            status=status.HTTP_400_BAD_REQUEST
-            if response.get("error", None)
-            else status.HTTP_200_OK)
-
-
-
