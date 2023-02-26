96e548b93e253ee274e92224bf94cc6b8d95c13d
obed <b.obed@yahoo.com>
2023-02-26 11:13:10 +0100

    Feature: Add Tutor Education Background

--- lektore/urls.py
+++ lektore/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('lektore-nimda/', admin.site.urls),
     path('lektore-api-v1/', include("app.urls")),
     path('api-auth/', include('rest_framework.urls')),
 
